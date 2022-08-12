056daf79545dbed7565feef8aea07f7b29abb6b3
Hafid Ikhsan Arifin <harifin315@gmail.com>
2022-08-12 16:21:25 +0700

    Initial Commit

---
+++ GameStore/GameStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStoreApp.swift
+//  GameStore
+//
+//  Created by Hafid Ikhsan Arifin on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
