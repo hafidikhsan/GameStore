5619a3b09e8c4a90ff35a162a2d02a6959032097
Hafid Ikhsan Arifin <harifin315@gmail.com>
2022-08-26 06:46:51 +0700

    Edit Home menu

--- GameStore/Views/Components/HomeMenuView.swift
+++ GameStore/Views/Components/HomeMenuView.swift
@@ -24,6 +24,7 @@ struct HomeMenuView: View {
             .frame(maxWidth: .infinity)
             .foregroundColor(Color.white)
             Text("\(genre)")
+                .foregroundColor(Color.black)
         }
     }
 }
