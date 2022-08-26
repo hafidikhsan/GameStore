05a5bb54c0b8fd9febaf161da9624dff08dbca6e
Hafid Ikhsan Arifin <harifin315@gmail.com>
2022-08-26 07:16:14 +0700

    Add Account View

--- GameStore/Views/Main/AccountView.swift
+++ GameStore/Views/Main/AccountView.swift
@@ -9,11 +9,31 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        NavigationView {
-            Text("Account Page")
-                .navigationTitle("Account")
+        ScrollView {
+            VStack(alignment: .leading) {
+                ZStack(alignment: .top) {
+                    Color("PrimaryColor").frame(height: 150)
+                    VStack {
+                        Spacer().frame(height: 80)
+                        Image("HafidIkhsanArifin")
+                            .resizable()
+                            .frame(width: 150, height: 150)
+                            .clipShape(Circle())
+                            .overlay(Circle()
+                                .stroke(Color.white, lineWidth: 3))
+                        Text("Hafid Ikhsan Arifin")
+                            .font(.title2.weight(.bold))
+                            .padding(.top)
+                        Text("Dicoding Academy iOS Developer")
+                            .font(.headline.weight(.regular))
+                            .foregroundColor(Color.gray)
+                            .padding(.bottom)
+                    }
+                }
+                .navigationTitle("About Me")
+                .navigationBarTitleDisplayMode(.inline)
+            }
         }
-        .navigationViewStyle(.stack)
     }
 }
 
